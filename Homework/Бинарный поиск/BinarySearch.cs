0e363ca04d1d3b0604c260dfa0650f8657d8d3f5
Arkaim234 <2006nikita2018@mail.ru>
2024-11-22 17:58:59 +0300

    Ух ты мой первый нирпрпмштяк!

---
+++ Homework/Бинарный поиск/BinarySearch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Бинарный_поиск
+{
+    internal class BinarySearch
+    {
+    }
+}
