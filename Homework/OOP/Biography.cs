8ee6d82a91b698534ba1dada39c1b85035fcbc2f
Arkaim234 <2006nikita2018@mail.ru>
2024-12-13 16:58:06 +0300

    OOP

---
+++ Homework/OOP/Biography.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.OOP
+{
+    internal record class Biography
+    {
+        public string _Name { get; init; }
+        public string _Surname { get; init; }
+        public int _Height { get; init; }
+        public int _Age { get; init; }
+        public Biography(string name, string surname, int height, int age)
+        {
+            _Name = name;
+            _Surname = surname;
+            _Height = height;
+            _Age = age;
+        }
+    }
+
+    public record struct BiographyChange
+    {
+        public int TownHall {  get; set; }
+        public int BulliedСhildrenInRoblox { get; set; }
+
+        public BiographyChange(int TownHall, int BulliedСhildrenInRoblox)
+        {
+            this.TownHall = TownHall;
+            this.BulliedСhildrenInRoblox = BulliedСhildrenInRoblox;
+        }
+    }
+}
--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Homework
+﻿using Homework.OOP;
+
+namespace Homework
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-         
+            Biography Nikita = new Biography("Никита", "Лопатин", 191, 18);
+            BiographyChange NikitaChange = new BiographyChange(13, 3);
+            Console.WriteLine($"Name: {Nikita._Name}," + 
+                $"Surname: {Nikita._Surname}, " +
+                $"Height: {Nikita._Height}, " +
+                $"Age: {Nikita._Age}");
+
+            Console.WriteLine($"TawnHall: {NikitaChange.TownHall}," +
+                $"BulliedСhildrenInRoblox: {NikitaChange.BulliedСhildrenInRoblox}");
         }
     }
 }
