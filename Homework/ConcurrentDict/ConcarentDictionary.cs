e9b216d8388fd6ed20cc12a6c6c8281f37330726
Arkaim234Ð <2006nikita2018@mail.ru>
2025-06-15 23:55:35 +0300

    чурка

---
+++ Homework/ConcurrentDict/ConcarentDictionary.cs
@@ -0,0 +1,100 @@
+﻿namespace ConcurentDictionary
+{
+    public class SpinLockDictionary<TKey, TValue>
+    {
+        private Dictionary<TKey, TValue> _dictionary = new Dictionary<TKey, TValue>();
+        private int _lockState = 0;
+
+        public bool TryAdd(TKey key, TValue value)
+        {
+            SpinWait spin = new SpinWait();
+
+            while (true)
+            {
+                if (TryAcquireLock())
+                {
+                    try
+                    {
+                        if (_dictionary.ContainsKey(key))
+                            return false;
+
+                        _dictionary.Add(key, value);
+                        return true;
+                    }
+                    finally
+                    {
+                        ReleaseLock();
+                    }
+                }
+                spin.SpinOnce();
+            }
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return _dictionary.TryGetValue(key, out value);
+        }
+
+        public bool TryRemove(TKey key, out TValue value)
+        {
+            SpinWait spin = new SpinWait();
+
+            while (true)
+            {
+                if (TryAcquireLock())
+                {
+                    try
+                    {
+                        if (_dictionary.TryGetValue(key, out value))
+                        {
+                            _dictionary.Remove(key);
+                            return true;
+                        }
+                        return false;
+                    }
+                    finally
+                    {
+                        ReleaseLock();
+                    }
+                }
+                spin.SpinOnce();
+            }
+        }
+
+        public bool TryUpdate(TKey key, TValue newValue)
+        {
+            SpinWait spin = new SpinWait();
+
+            while (true)
+            {
+                if (TryAcquireLock())
+                {
+                    try
+                    {
+                        if (_dictionary.ContainsKey(key))
+                        {
+                            _dictionary[key] = newValue;
+                            return true;
+                        }
+                        return false;
+                    }
+                    finally
+                    {
+                        ReleaseLock();
+                    }
+                }
+                spin.SpinOnce();
+            }
+        }
+
+        private bool TryAcquireLock()
+        {
+            return Interlocked.CompareExchange(ref _lockState, 1, 0) == 0;
+        }
+
+        private void ReleaseLock()
+        {
+            Volatile.Write(ref _lockState, 0);
+        }
+    }
+}
---
+++ Homework/ConcurrentDict/ThreadSafeBinarTree.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+
+class ThreadSafeBinaryTree
+{
+    private class Node
+    {
+        public int Value;
+        public Node Left, Right;
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+
+    private Node root;
+    private ReaderWriterLockSlim treeLock = new ReaderWriterLockSlim();
+
+    public void Add(int value)
+    {
+        treeLock.EnterWriteLock();
+        try
+        {
+            root = AddRecursive(root, value);
+        }
+        finally
+        {
+            treeLock.ExitWriteLock();
+        }
+    }
+
+    private Node AddRecursive(Node node, int value)
+    {
+        if (node == null)
+            return new Node(value);
+
+        if (value < node.Value)
+            node.Left = AddRecursive(node.Left, value);
+        else if (value > node.Value)
+            node.Right = AddRecursive(node.Right, value);
+        // Дубликаты игнорируем
+
+        return node;
+    }
+
+    public bool Contains(int value)
+    {
+        treeLock.EnterReadLock();
+        try
+        {
+            return ContainsRecursive(root, value);
+        }
+        finally
+        {
+            treeLock.ExitReadLock();
+        }
+    }
+
+    private bool ContainsRecursive(Node node, int value)
+    {
+        if (node == null)
+            return false;
+
+        if (value == node.Value)
+            return true;
+        else if (value < node.Value)
+            return ContainsRecursive(node.Left, value);
+        else
+            return ContainsRecursive(node.Right, value);
+    }
+
+    public void Print()
+    {
+        treeLock.EnterReadLock();
+        try
+        {
+            PrintRecursive(root, 0);
+        }
+        finally
+        {
+            treeLock.ExitReadLock();
+        }
+    }
+
+    private void PrintRecursive(Node node, int indent)
+    {
+        if (node != null)
+        {
+            PrintRecursive(node.Right, indent + 4);
+            Console.WriteLine(new string(' ', indent) + node.Value);
+            PrintRecursive(node.Left, indent + 4);
+        }
+    }
+}
+
